d626851c6b5703fb1126f3b5e8bd612dcb5dbc68
ppapuli <peter.papuli@gmail.com>
2023-03-08 15:57:12 -0500

    file restructure, path generalize

---
+++ scripts/gui.py
@@ -0,0 +1,37 @@
+# import main
+
+def CreateGUI():    
+    import tkinter as tk
+
+    # Create the main window
+    window = tk.Tk()
+
+    # Set window title
+    window.title("Land Suggestor")
+
+    # Create a label and set its text
+    label = tk.Label(text="MtGA Deck Import")
+    label.pack()
+
+    # Create an input text box
+    text_box = tk.Entry(text="1 Plains\n1 Island\n1 Swamp\n1 Mountain\n1 Forest", width=50)
+    text_box.pack()
+
+    # Retreive the text entered by the user
+    def retreive_text():
+        global imported_deck
+        imported_deck = text_box.get()
+        main.ConvertDeckList(imported_deck)
+
+    # Create a button and set its text
+    button = tk.Button(text="Import", command=retreive_text)
+    button.pack()
+
+    # Start the main loop
+    window.mainloop()
+
+# def GetDeckList():
+#     return deck_list
+
+if __name__ == '__main__':
+    CreateGUI()
---
+++ scripts/import_data_source.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import os
+
+"""
+    TODO: 
+        SET UP API CALL TO SCRYFALL AND DELETE default-cards.json file
+"""
+
+# Get the absolute path of the current working directory
+current_dir = os.path.abspath(os.getcwd())
+
+# Get the absolute path of the parent directory
+parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
+
+# Set file path to the json file with card data downloaded directly from scryfall
+file_path = os.path.join(parent_dir, 'data', 'default-cards-20221230100442.json')
+
+print(file_path)
+
+df_source = pd.read_json(file_path, orient='records')
+
+# Subset the source data into only fields we care about
+lexicon = df_source[[
+    'id',
+    'name',
+    'mana_cost',
+    'cmc',
+    'type_line',
+    'oracle_text',
+    'power',
+    'toughness',
+    'colors',
+    'color_identity',
+    'produced_mana',
+    'keywords',
+    'set',
+    'rarity',
+    'flavor_text',
+    'edhrec_rank',
+    'arena_id',
+    'loyalty',
+    'card_faces',
+]]
+
+lexicon.to_feather('mtg_card_database')
+
+def get_card_data():
+    return pd.read_feather(path='mtg_card_database')
